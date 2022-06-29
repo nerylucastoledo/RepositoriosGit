06978115f3cc5174799918b81b4e44c32036ca40
lucasnerytoledo <lucas.leite@petlove.com.br>
2022-06-29 14:21:28 -0300

    inserindo header, icon lupa e logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+
+    <Header />
+
     <main id="main">
         <transition mode="out-in">
           <router-view />
@@ -7,3 +10,40 @@
     </main>
   </div>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+body {
+  background-color: #1C1B1F;
+}
+
+* {
+  padding: 0px;
+  margin: 0px;
+  font-family: 'Roboto';
+  text-decoration: none;
+  font-weight: 400;
+  list-style: none;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0px;
+  position: relative;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+    <header>
+        <nav class="container navbar">
+            <router-link to="/" id="logo">
+                <img src="../assets/logo.png" alt="Logo">
+            </router-link>
+
+            <ul>
+                <li>
+                    <router-link to="/">Pesquisa</router-link>
+                </li>
+                <li>
+                    <router-link to="/favoritos">Favoritos</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+header {
+    background-color: #49454F;
+    border-radius: 0 0 12px 12px;
+    border-bottom: 1px solid #938F99;
+    padding: 10px 8px;
+}
+
+.navbar {
+    display: flex;
+    align-items: center;
+}
+
+.navbar ul {
+    width: 100%;
+    text-align: center;
+}
+
+.navbar ul li {
+    display: inline-block;
+    margin: 0 30px;
+}
+
+.navbar ul li a {
+    color: #E6E1E5;
+    position: relative;
+}
+
+.navbar ul li a.router-link-exact-active::after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 1px;
+    background: #E6E1E5;
+    margin: 20px 0;
+    position: absolute;
+}
+
+
+</style>
